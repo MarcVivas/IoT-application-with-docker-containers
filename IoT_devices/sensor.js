ae3b7e3fa9acffde4c1068459d20c792927e2b03
MarcVivas <vivasmarc001@gmail.com>
2022-11-19 10:00:36 +0100

    Sensor code refactor

--- IoT_devices/sensor.js
+++ IoT_devices/sensor.js
@@ -17,46 +17,51 @@ function main() {
 
     // -----------------------------------------------------------
     // Read csv
-    let collected_data = (require("fs").readFileSync("./data2.csv", "utf8")).split("\r");
+    let collectedData = (require("fs").readFileSync("./data2.csv", "utf8")).split("\r");
     // Remove the column name (Temperature) from the array
-    collected_data.shift();
+    collectedData.shift();
     // -----------------------------------------------------------
 
-
-
-    let topic = 'temperature';
 
     // Once connected ...
     client.on('connect', () => {
 
-        // Sensor connected
+        // Sensor connected!
         console.log('Sensor '+ process.env.HOSTNAME + ' is now connected to the MQTT broker');
 
-        // -----------------------------------------------------------
-        // Every 10 seconds publish a message to the temperature topic!
-
-        let i = 0; // Counter to iterate the collected data
-
-        setInterval(() => {
-
-            let temperatureId = i++ % collected_data.length;
-
-            const message = JSON.stringify({
-                "sensorId": process.env.HOSTNAME,
-                "temperature": collected_data[temperatureId],
-                'temperatureId': temperatureId,
-                'collectedAt': new Date()
-            });
-
-            client.publish(topic, message, { qos: 0 }, (error) => {
-                if (error) {
-                    console.error(error);
-                }
-            });
-
-        }, 10 * 1000);
-        // -----------------------------------------------------------
+        publishCollectedData(collectedData, 'temperature', client);
 
     });
 
+}
+
+/**
+ * Every 10 seconds publish a message to the temperature topic!
+ * @param collectedData
+ * @param topic
+ * @param client
+ * @return void
+ */
+function publishCollectedData(collectedData, topic, client){
+
+    let i = 0; // Counter to iterate the collected data
+
+    setInterval(() => {
+
+        let temperatureId = i++ % collectedData.length;
+
+        const message = JSON.stringify({
+            "sensorId": process.env.HOSTNAME,
+            "temperature": collectedData[temperatureId],
+            'temperatureId': temperatureId,
+            'collectedAt': new Date()
+        });
+
+        client.publish(topic, message, { qos: 0 }, (error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+
+    }, 10 * 1000);
 }
